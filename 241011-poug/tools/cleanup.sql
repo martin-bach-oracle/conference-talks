3e6b98daa92cf104cbb99a58a12cf286a495d7f1
martin bach <martin.b.bach@oracle.com>
2024-08-16 14:16:35 +0200

    refactor code and locations

--- 241011-poug/tools/cleanup.sql
+++ 241011-poug/tools/cleanup.sql
@@ -9,3 +9,8 @@ drop table if exists demo_things cascade constraints purge;
 drop table if exists demo_warehouse cascade constraints purge;
 drop table if exists  demo_thing_stock cascade constraints purge;
 drop view if exists thing;
+
+drop mle module if exists VALIDATOR_MODULE;
+drop mle module if exists DEMO_THINGS_MODULE;
+
+drop package if exists DEMO_THINGS_PKG;
