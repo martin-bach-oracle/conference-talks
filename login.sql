56cb56b943889e8da4ece849c3ec4ea718ebfd97
martin bach <martin.b.bach@oracle.com>
2025-09-02 10:38:18 +0200

    feat: sqlcl project init

---
+++ login.sql
@@ -0,0 +1,7 @@
+set sqlformat ansiconsole
+set sqlprompt "@|red _USER|@@@|green _O_RELEASE|@@|blue   🚥  >|@ "
+
+set statusbar on
+set statusbar add project
+set statusbar add git
+set statusbar add cwd
